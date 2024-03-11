5dceb256d00715436f310a7c95f6ffef1e7bfeb6
Pranav Singh <pranavsingh02@hotmail.com>
2024-03-12 00:01:16 +0530

    Add custom paritioner
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

---
+++ src/com/kafka/partitioner/CustomParitioner.java
@@ -0,0 +1,21 @@
+package com.kafka.partitioner;
+
+import java.util.Map;
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+
+public class CustomParitioner implements Partitioner {
+    private static final int PARTITION_COUNT=50;
+
+    @Override
+    public void configure(Map<String, ?> configs){}
+
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster){
+        Integer keyInt=Integer.parseInt(key.toString());
+        return keyInt % PARTITION_COUNT;
+    }
+
+    @Override
+    public void close(){}
+}
