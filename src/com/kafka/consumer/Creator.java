794c7e25802338ae56370ac3e90d5c97cd666edd
Pranav Singh <pranavsingh02@hotmail.com>
2024-03-12 00:00:10 +0530

    add consumer
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

---
+++ src/com/kafka/consumer/Creator.java
@@ -0,0 +1,30 @@
+package com.kafka.consumer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import com.kafka.constants.IKafkaConstants;
+
+public class Creator {
+    public static Consumer<Long, ?> createConsumer(){
+        final Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.KAFKA_BROKERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, IKafkaConstants.KAFKA_BROKERS);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, IKafkaConstants.MAX_POLL_RECORDS);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, IKafkaConstants.OFFSET_REST_EARLIER);
+
+        final Consumer<Long, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(IKafkaConstants.TOPIC_NAME));
+        return consumer;
+
+    }
+}
